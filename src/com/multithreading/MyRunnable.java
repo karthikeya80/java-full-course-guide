1d28b4e0cfe2b15209d676ca6b71c8ac6d3917a8
karthikeya80 <karthikeya80@email.com>
2025-07-06 11:10:38 +0530

    Corrected logic MyRunnable.java

--- src/com/multithreading/MyRunnable.java
+++ src/com/multithreading/MyRunnable.java
@@ -4,12 +4,20 @@ public class MyRunnable extends Thread {
 
 	public static void main(String[] args) {
 
-		Thread t = new Thread();
+		MyRunnable t = new MyRunnable();
 		t.start();
 
 	}
 	public void run() {
-		System.out.println("Thread running via Runnable interface");
+		for(int i=0; i<=3;i++) {
+			System.out.println("Thread running : " + i);
+			try {
+				Thread.sleep(1200);
+			}catch(InterruptedException e) {
+				
+			}
+		}
+		
 	}
 
 }
