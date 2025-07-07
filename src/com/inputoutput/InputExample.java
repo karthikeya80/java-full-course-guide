26da16a8075eb631ae7c85c0afc5712691c81bf1
karthikeya80 <karthikeya80@email.com>
2025-07-07 08:51:53 +0530

    added InputExample.java class

---
+++ src/com/inputoutput/InputExample.java
@@ -0,0 +1,16 @@
+package com.inputoutput;
+
+import java.util.Scanner;
+
+public class InputExample {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		
+		  String name = sc.nextLine();
+	        System.out.println("Hello : " + name);
+
+	}
+
+}
